7414ea7d2e2dcdf872680011c621a106ec35b643
SuperCoder7979 <k.pranav@gmail.com>
2020-03-31 20:50:22 -0400

    Initial commit

---
+++ src/main/java/supercoder79/fatecrates/FateCrates.java
@@ -0,0 +1,10 @@
+package supercoder79.fatecrates;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FateCrates implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
